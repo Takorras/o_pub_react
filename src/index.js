1b5dcb782459c54fd6bfe8b1f7dda1fa9c6b5518
Takorras <sy.empress0321@gmail.com>
2017-12-10 15:53:04 +0900

    [modify]storeをひもづけ

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import opubApp from './reducers';
 import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(opubApp);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
