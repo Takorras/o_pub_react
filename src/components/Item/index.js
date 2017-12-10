65fe93eed51c910b46e5b704a5057a796de68cdd
Takorras <sy.empress0321@gmail.com>
2017-12-10 22:31:27 +0900

    [modify]レイアウトの変更

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { Label } from 'semantic-ui-react';
 
+const LabelWrapper = styled.span`
+  margin-left: 36px;
+`
+
+const LiWrapper = styled.li`
+  margin-bottom: 16px;
+`
+
 const Item = ({ text, point }) => (
-  <li>
+  <LiWrapper>
     <span>{text}</span>
-    <Label>{point}</Label>
-  </li>
+    <LabelWrapper>
+      <Label color='blue'>{point} オッ</Label>
+    </LabelWrapper>
+  </LiWrapper>
 );
 
 Item.propTypes = {
